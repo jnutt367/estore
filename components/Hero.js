9c581b8385fb4af151488a4dc00772f8010abd66
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-04-23 10:15:08 -0400

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -15,9 +15,9 @@ const Hero = () => {
       />
       <div className="bg-gray-900 bg-opacity-50 flex items-center h-full absolute w-full z-10">
         <div className="px-10 max-w-xl">
-          <h2 className="font-serif text-2xl text-white font-semibold">Tech Nutt (An E-commerce Store)</h2>
+          <h2 className="font-serif text-2xl text-white font-semibold">The WORD of GOD: "Risen"</h2>
           <p className="mt-2 text-white">
-           Find <i className="font-serif text-indigo-200">your favorite Technology's</i> Gear here! Data Synched with Realm sync.
+           Find <i className="font-serif text-indigo-200">Landing page for OUR</i> Bible App!! Data Synched with Realm sync.
           </p>
           <Link href={`/products`}>
             <button className="font-serif flex items-center mt-4 px-3 py-2 bg-indigo-600 text-white text-sm uppercase font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
